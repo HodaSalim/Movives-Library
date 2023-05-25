0df647a64c7abf9521e28fb8900f5fd498db9531
HodaSalim <hi@hodasalim.com>
2023-05-25 16:17:15 +0300

    added filteration by title and by stars

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Header } from "./components/Header";
 import { useState } from "react";
 import movies from "./data/data.json";
 import { NewMovie } from "./components/NewMovie";
 import { MoviesList } from "./components/MoviesList";
 import { onVisibility } from "./components/utils/onVisiblity";
+import { Filters } from "./components/Filters";
 
 function App() {
   const [moviesList, setMoviesList] = useState(movies);
+
+  const onFilter = useCallback((query, stars) => {
+    console.log(typeof stars);
+    const filteredList = movies
+      .filter((movie) => movie.title.toLowerCase().includes(query))
+      .filter((movie) => (stars ? movie.rating === Number(stars) : movie));
+
+    setMoviesList(filteredList);
+  }, []);
+
   return (
     <>
       <Header />
       <NewMovie setMoviesList={setMoviesList} />
+      <Filters onFilter={onFilter} />
       <MoviesList movies={moviesList} />
       <div className="overlay hidden" onClick={onVisibility}></div>
     </>
